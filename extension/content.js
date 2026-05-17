631d4856b164f46824ea63992443d1514e12ad91
Aarav Bhatia <aarav.bhatia@somaiya.edu>
2026-05-17 17:11:28 +0530

    deployment

--- extension/content.js
+++ extension/content.js
@@ -45,7 +45,7 @@ async function translateCurrentSong(trackName, artist) {
   showLoadingIndicator();
 
   try {
-    const res = await fetch('http://localhost:3000/translate', {
+    const res = await fetch('https://your-project-name.vercel.app/translate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
